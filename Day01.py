550bafdecdc441d097dec24cecd47a70d697b485
DiegoR <128084117+DLRedd@users.noreply.github.com>
2024-10-10 13:52:25 +0100

    Days 01 y 02
    dale manin

---
+++ Day01.py
@@ -0,0 +1,4 @@
+nom = input("¿Cual es tu nombre?");
+apll = input("¿Cual es tu apellido?");
+
+print("Tu nombre es " + nom + ", y tu apellido es " + apll);
---
+++ Day02.py
@@ -0,0 +1,8 @@
+bill = (input("How many is the total bill? $ "))
+tip = (input("How many do you want to tip?"))
+billy = (type("amorcete"));
+
+
+len("Hello", 22, "caca", 222,22); #esto es una funcion y sus argumentos
+
+print ();
